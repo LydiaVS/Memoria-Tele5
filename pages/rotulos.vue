1c6470aa0b4b9c521f0a69a23c5338ff5a4e2aa2
LydiaVS <lidia.vazquez98@gmail.com>
2022-06-02 20:48:19 +0200

    casi

--- pages/rotulos.vue
+++ pages/rotulos.vue
@@ -3,13 +3,36 @@
   <MainMenu/>
   <div class="caja">
     <h1>Rótulos</h1>
-    <p>Para la realización de los rótulos se empleó una retícula con las que poder ir estructurando las diferentes piezas viasuales.</p>
+    <p>Para la realización de los rótulos se empleó una retícula con las que poder ir estructurando las diferentes piezas viasuales.
+      <br> A partir de aquí decidimos crear pastillas solidas jugando con una jerarquía de color para llamar la atención sobre los diferentes elementos informativos.
+    </p>
     <img class="tamaño" src="../assets/img/reticula.jpg" alt="">
+    <h2>Pantallas con guías</h2>
+    <img class="tamaño" src="../assets/img/pantallasguias.jpg" alt="">
+    <img class="tamaño" src="../assets/img/doblespantallas.jpg" alt="">
+    <img class="tamaño" src="../assets/img/doblespantallas-guias.jpg" alt="">
+    <img class="tamaño" src="../assets/img/rotulo1.jpg" alt="">
+    <img class="tamaño" src="../assets/img/rotulo2.jpg" alt="">
+
   </div>
 </div>
   
 </template>
+<script>
+import VideoPlayer from 'nuxt-video-player'
+
+//require('nuxt-video-player/src/assets/css/main.css')
+
+export default{
+components: {
+    VideoPlayer
+}
+
+}
+</script>
 <style lang="sass">
   .tamaño
     @apply max-w-lg
+  p
+    @apply mb-10
 </style>
